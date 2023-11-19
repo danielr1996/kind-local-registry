e3795ce201faeb7d13f2599383b66eee5c9c3849
Daniel Richter <danielrichter@posteo.de>
2023-11-19 22:37:23 +0100

    feat: enable port forwarding for ingress controller
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- kind.sh
+++ kind.sh
@@ -23,6 +23,21 @@ fi
 cat <<EOF | kind create cluster --name $CLUSTERNAME --kubeconfig $KUBECONFIG --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
 containerdConfigPatches:
 - |-
   [plugins."io.containerd.grpc.v1.cri".registry]
@@ -64,3 +79,5 @@ data:
     host: "localhost:${reg_port}"
     help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
 EOF
+
+KUBECONFIG=$KUBECONFIG kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
